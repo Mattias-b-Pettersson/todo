291d89ff7e063e210c92a767a96fce68d2286bf3
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-15 15:52:05 +0000

    Rafactor divs and td to row and col in signupform

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -6,6 +6,7 @@ import SignInImage from "../../assets/SignInImage.jpg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faLock, faKey } from '@fortawesome/free-solid-svg-icons'
 import styles from "../../styles/SignUpInForm.module.css"
+import { Link } from 'react-router-dom';
 
 const SignUpForm = () => {
     return (
@@ -16,32 +17,36 @@ const SignUpForm = () => {
                     <Row className="justify-content-center">
                         <Col md='12' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
                             <Form >
-                            <h1 className="text-center fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</h1>
-                            <div className="d-flex flex-row align-items-center mb-4 ">
-                                <Form.Group className="mb-3" controlId="username">
+                                <h1 className="text-center fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</h1>
+
+                                <Form.Group className="mb-4" controlId="username">
                                     <Form.Label className="d-none">Username</Form.Label>
-                                    <td><FontAwesomeIcon icon={faUser} className="me-2" /></td>
-                                    <td><Form.Control type="username" placeholder="Enter username" name="username" /></td>
+                                    <Row className="">
+                                        <Col xs={1}><FontAwesomeIcon icon={faUser} className="me-2" /></Col>
+                                        <Col xs={11}><Form.Control type="username" placeholder="Enter username" name="username" /></Col>
+                                    </Row>
                                 </Form.Group>
-                            </div>
-
-                            <div className="d-flex flex-row align-items-center mb-4">
-                                <Form.Group className="mb-3" controlId="password1">
-                                        <Form.Label className='d-none'>Password</Form.Label>
-                                        <td><FontAwesomeIcon icon={faLock} className="me-2" /></td>
-                                    <td><Form.Control type="password1" placeholder="Enter password" name="password1" /></td>
+                                
+                                <Form.Group className="mb-4" controlId="password1">
+                                    <Form.Label className='d-none'>Password</Form.Label>
+                                    <Row className="">
+                                        <Col xs={1}><FontAwesomeIcon icon={faLock} className="" /></Col>
+                                        <Col xs={11}><Form.Control className="md-auto" type="password1" placeholder="Enter password" name="password1" /></Col>
+                                    </Row>
                                 </Form.Group>
-                            </div>
 
-                            <div className="d-flex flex-row align-items-center mb-4">
+                            
                                 <Form.Group className="mb-3" controlId="password2">
-                                    
-                                        <Form.Label className='d-none'>Confirm password</Form.Label>
-                                        <td><FontAwesomeIcon icon={faKey} className="me-1" /></td>
-                                        <td><Form.Control type="password2" placeholder="Confirm password" name="password2" /></td>
-                                </Form.Group>
-                            </div>
 
+                                    <Form.Label className='d-none'>Confirm password</Form.Label>
+
+                                    <Row className="">
+                                        <Col xs={1}><FontAwesomeIcon icon={faKey} className="me-1" /></Col>
+                                        <Col xs={11}><Form.Control type="password2" placeholder="Confirm password" name="password2" /></Col>
+                                    </Row>
+                                </Form.Group>
+                                
+                                <p>Already have an account? Sign in <Link to="/signin">here</Link></p>
 
                                 <Button className={`mb-4 ${styles.btn}`} size='lg'>Register</Button>
                             </Form>
