d02d5a2ec4a6ab729326dd843268d66bc08d4df0
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-15 19:53:42 +0000

    Add auto refresh for access token

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,5 +1,7 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
+import { axiosReq, axiosRes } from '../api/axiosDefaults';
+import { useNavigate } from 'react-router-dom';
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -9,6 +11,7 @@ export const useSetCurrentUser = () => useContext(SetCurrentUserContext)
 
 const CurrentUserProvider = ({children}) => {
     const [currentUser, setCurrentUser] = useState(null);
+    const navigate = useNavigate()
 
     const handleMount = async () => {
         try {
@@ -23,6 +26,48 @@ const CurrentUserProvider = ({children}) => {
         handleMount();
     }, [])
 
+
+    useMemo(() => {
+        axiosReq.interceptors.request.use(
+            async (config) => {
+                try {
+                    await axios.post("/dj-rest-auth/token/refresh/");
+                } catch (err) {
+                    setCurrentUser((prevCurrentUser) => {
+                        if (prevCurrentUser) {
+                            navigate("/signin");
+                        }
+                        return null;
+                    });
+                    return config;
+                }
+                return config;
+            },
+            (err) => {
+                return Promise.reject(err);
+            }
+        );
+
+        axiosRes.interceptors.response.use(
+            (response) => response,
+            async (err) => {
+                if (err.response?.status === 401) {
+                    try {
+                        await axios.post("/dj-rest-auth/token/refresh/");
+                    } catch (err) {
+                        setCurrentUser((prevCurrentUser) => {
+                            if (prevCurrentUser) {
+                                navigate("/signin");
+                            }
+                            return null;
+                        });
+                    }
+                    return axios(err.config);
+                }
+                return Promise.reject(err);
+            }
+        );
+    }, [navigate]);
     return (
         <CurrentUserContext.Provider value={currentUser}>
             <SetCurrentUserContext.Provider value={setCurrentUser}>
