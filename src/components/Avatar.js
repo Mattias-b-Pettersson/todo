71da595a5c6b66bc194b1b5a3c32640c9b1cb943
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-15 21:31:34 +0000

    Update css for avatar component

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,7 +3,7 @@ import styles from "../styles/Avatar.module.css"
 
 const Avatar = ({ src, height = 45, text }) => {
     return (
-        <span>
+        <span className={`${styles.AvatarSpan}`}>
             <img className={`${styles.Avatar}`} src={src} height={height} width={height} alt="Avatar" />
             {text}
         </span>
