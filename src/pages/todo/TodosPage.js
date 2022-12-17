3154a38a18771e5fdb506604b9f89b391442c185
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-17 13:43:47 +0000

    Add Todos page

---
+++ src/pages/todo/TodosPage.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import { Row, Col, Card } from 'react-bootstrap';
+import { axiosReq } from '../../api/axiosDefaults';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { Todo } from './Todo';
+import loading from "../../assets/loading.gif"
+import { useLocation } from 'react-router-dom';
+
+
+
+export const TodosPage = () => {
+    const [todo, setTodo] = useState({ results: [] })
+    const [hasLoaded, setHasLoaded] = useState(false)
+    const currentUser = useCurrentUser();
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        // Get the todo that with id from the URL on mount
+        const fetchOwnedPosts = async () => {
+            try {
+                const { data } = await axiosReq.get(`/todos/?owner=${currentUser?.profile_id}`)
+                setTodo(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchOwnedPosts();
+        setHasLoaded(true)
+    }, [currentUser?.profile_id, pathname])
+
+
+    return (
+        <Row fluid="true" className="justify-content-center">
+            <Col xl={8} lg={10} className='mt-3'>
+                <Row>
+                    {currentUser ? (
+                        hasLoaded ? (
+                            <Card>
+                            <h1 className="mt-4">Todo</h1>
+                            <Card.Body>
+                                    {todo.results.map(result => <div key={result.id} className='mb-4'>
+                                    < Todo {...result} setTodo={setTodo} />
+                                </div>)}
+                            </Card.Body>
+                            </Card>
+                        ) : (
+                            <img src={loading} height={102} alt="loading..." />
+                        )
+                    ) : (
+                        <p>Please sign in or create an account</p>
+                    )}
+                    
+                </Row>
+            </Col>
+        </Row>
+    )
+}
