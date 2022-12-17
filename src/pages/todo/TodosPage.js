cb215ca1a0c8c171bbbb21797952ae0544932db2
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-17 21:53:28 +0000

    Add search options for todos page

--- src/pages/todo/TodosPage.js
+++ src/pages/todo/TodosPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Row, Col, Card } from 'react-bootstrap';
+import { Row, Col, Card, Form } from 'react-bootstrap';
 import { axiosReq } from '../../api/axiosDefaults';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { Todo } from './Todo';
@@ -13,21 +13,36 @@ export const TodosPage = () => {
     const [hasLoaded, setHasLoaded] = useState(false)
     const currentUser = useCurrentUser();
     const { pathname } = useLocation();
+    const [searchFields, setSearchFields] = useState({
+        search: "",
+        ordering: "",
+    });
+    const { search, ordering } = searchFields;
 
     useEffect(() => {
-        // Get the todo that with id from the URL on mount
-        const fetchOwnedPosts = async () => {
+        // Fetches all todos that are assigned to the current user and sets the state to todo with data that is returned from the API and sets hasLoaded to true so that the loading gif is not shown anymore
+        const fetchTodos = async () => {
             try {
-                // const { data } = await axiosReq.get(`/todos/?owner=${currentUser?.profile_id}`)
-                const { data } = await axiosReq.get(`/todos/?owner=&assigned=${currentUser?.profile_id}`)
+                const { data } = await axiosReq.get(`/todos/?owner=&assigned=${currentUser.profile_id}&search=${search}`)
                 setTodo(data)
+                setHasLoaded(true)
             } catch (error) {
                 console.log(error)
             }
         }
-        fetchOwnedPosts();
-        setHasLoaded(true)
-    }, [currentUser?.profile_id, pathname])
+        setHasLoaded(false);
+        fetchTodos();
+        
+    }, [currentUser, pathname, search])
+
+    const handleChange = (e) => {
+        // handles the change of the search and ordering fields
+        setSearchFields({
+            ...searchFields,
+            [e.target.name]: e.target.value
+        })
+
+    }
 
 
     return (
@@ -35,18 +50,29 @@ export const TodosPage = () => {
             <Col xl={8} lg={10} className='mt-3'>
                 <Row>
                     {currentUser ? (
-                        hasLoaded ? (
                             <Card>
-                            <h1 className="mt-4">Todo</h1>
+                                <h1 className="mt-4">Todo</h1>
+                                
+                                <Form onSubmit={(event)=>event.preventDefault}>
+                                    <p className='mb-1'>Order by:</p>
+                                    <Form.Control name="search" as="input" onChange={handleChange} value={search} placeholder="Search..." />
+                                    <Form.Control name="ordering" as="select" onChange={handleChange} value={ordering}>
+                                        <option value="status">Ascending status</option>
+                                        <option value="-status">Descending status</option>
+                                        <option value="priority">Ascending priority</option>
+                                        <option value="-priority">Descending priority</option>
+                                    </Form.Control>
+                                </Form>
+                            {hasLoaded ? (
                             <Card.Body>
                                 {todo.results.map(result => <div key={result.id} className='mb-4'>         
                                         < Todo {...result} setTodo={setTodo} />
                                 </div>)}
                             </Card.Body>
-                            </Card>
                         ) : (
-                            <img src={loading} height={102} alt="loading..." />
-                        )
+                            <img src={loading} height={102} width={102} alt="loading..." className='mx-auto my-5'/>
+                        )}
+                        </Card>
                     ) : (
                         <p>Please sign in or create an account</p>
                     )}
