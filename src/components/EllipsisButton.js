ab4061857fd18d20c0d658eb5d759c704954709b
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-18 13:05:49 +0000

    Fix ellipsisButton bug, edit link didn't cover btn

--- src/components/EllipsisButton.js
+++ src/components/EllipsisButton.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { faEllipsisV, faFilePen, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Dropdown } from 'react-bootstrap';
+import { Button, Dropdown } from 'react-bootstrap';
 import styles from "../styles/EllipsisButton.module.css"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { axiosRes } from '../api/axiosDefaults';
 
 export const EllipsisButton = ({ isType, id, handleDelete }) => {
     const [typelink, setTypelink] = useState("")
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (isType === "todo") {
@@ -18,28 +19,34 @@ export const EllipsisButton = ({ isType, id, handleDelete }) => {
     }, [isType])
             
 
+    const handleChange = (e) => {
+        e.preventDefault();
+        navigate(`${typelink}/${id}/edit`)
+    }
+
     const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-        <a
+        <Button
+          variant='link'
+          className=""
           href=""
           ref={ref}
           onClick={(e) => {
             e.preventDefault();
             onClick(e);
           }}
-          className="text-decoration-none color-black"
         >
           {children}
-          <FontAwesomeIcon icon={faEllipsisV} className="" />
-        </a>
+          <FontAwesomeIcon icon={faEllipsisV} className="color-black" />
+        </Button>
         ));
 
   return (
     <Dropdown drop="left">
-      <Dropdown.Toggle as={CustomToggle} variant="success" id="dropdown-basic" >
+      <Dropdown.Toggle as={CustomToggle} id="dropdown-basic" >
       </Dropdown.Toggle>
 
       <Dropdown.Menu className={styles.menuOuter}>
-        <Dropdown.Item className={styles.menuItem}><Link to={`${typelink}/${id}/edit`}><FontAwesomeIcon icon={faFilePen} alt="edit" /></Link></Dropdown.Item>
+        <Dropdown.Item className={styles.menuItem} onClick={handleChange}><FontAwesomeIcon icon={faFilePen} alt="edit" /></Dropdown.Item>
         <Dropdown.Divider className='p-0 m-0'/>
         <Dropdown.Item  onClick={handleDelete} className={styles.menuItem}><FontAwesomeIcon icon={faTrashCan} alt="delete" /></Dropdown.Item>
       </Dropdown.Menu>
