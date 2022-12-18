f32eb9bae5b0fe9dc7091a59ab724619b1f682b4
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-18 16:17:22 +0000

    Add comments to postpage

--- src/pages/todo/TodoPage.js
+++ src/pages/todo/TodoPage.js
@@ -3,25 +3,38 @@ import { Row, Col, Card } from 'react-bootstrap';
 import { useParams } from 'react-router-dom'
 import { axiosReq } from '../../api/axiosDefaults';
 import { Todo } from './Todo';
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Comment } from '../comments/Comment';
+import loading from "../../assets/loading.gif"
 
 
 export const TodoPage = () => {
     const { id } = useParams();
     const [todo, setTodo] = useState({ results: [] })
+    const [hasLoaded, setHasLoaded] = useState(false);
+
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
     
     useEffect(() => {
 
         // Get the todo that with id from the URL on mount
         const handleMount = async () => {
             try {
-                const [{ data: todo }] = await Promise.all([
+                const [{ data: todo }, { data: comments}] = await Promise.all([
                     axiosReq.get(`/todo/${id}/`),
+                    axiosReq.get(`/comments/?todo=${id}`),
                 ])
-                setTodo({ results: [todo] })
+                setTodo({ results: [todo] });
+                setComments(comments);
+                setHasLoaded(true);
             } catch (error) {
                 console.log(error)
             }
         }
+        setHasLoaded(false);
         handleMount();
 
     },[id, todo])
@@ -30,14 +43,35 @@ export const TodoPage = () => {
     return (
         <Row fluid="true" className="justify-content-center">
             <Col xl={8} lg={10} className='mt-3'>
-                <Row>
-                    <Card>
-                        <h1 className="mt-4">Todo</h1>
-                        <Card.Body>
-                            <Todo {...todo.results[0]} setTodo={setTodo} />
-                        </Card.Body>
-                    </Card>
-                </Row>
+                <Card>
+                    {hasLoaded ? (
+                    <Card.Body>
+                        <Todo {...todo.results[0]} setTodo={setTodo} />
+                        {currentUser ? (
+                            <CommentCreateForm
+                            profile_id={currentUser.profile_id}
+                            profileImage={profile_image}
+                            todo={id}
+                            setTodo={setTodo}
+                            setComments={setComments}
+                            />
+                        ) : comments.results.length ? (
+                            "Comments"
+                        ) : null}
+                        {comments.results.length ? (
+                            comments.results.map((comment) => (
+                                <Comment
+                                key={comment.id}
+                                {...comment}
+                                setComments={setComments}
+                                />
+                            ))
+                        ) : <span className='m3-4'>No comments yet...</span>}
+                    </Card.Body>
+                    ) : (
+                        <img className="mx-auto my-4" src={loading} width="102" height="102" alt="loading" />
+                    )}
+                </Card>
             </Col>
         </Row>
     )
