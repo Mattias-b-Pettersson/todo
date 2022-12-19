0fd14a5ba25d5aad2422280939eec3e1dca25a62
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-19 20:49:05 +0000

    Add edit username btn to  display username form

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -4,13 +4,17 @@ import { useParams } from 'react-router-dom';
 import { axiosReq } from '../../api/axiosDefaults';
 import loading from "../../assets/loading.gif"
 import Avatar from '../../components/Avatar';
-import { EllipsisButton } from '../../components/EllipsisButton';
+import { EllipsisProfileButton } from '../../components/EllipsisButton';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from '../../styles/ProfilePage.module.css';
+import UsernameForm from './UsernameForm';
 
 export const ProfilePage = () => {
 
-    const { id } = useParams();
+    const currentUser = useCurrentUser();
 
+    const { id } = useParams();
+    const [showEditUserNameForm, setShowEditUserNameForm] = useState(false);
     const [profile, setProfile] = useState({ results: [] })
     const [hasLoaded, setHasLoaded] = useState(false);
 
@@ -28,7 +32,7 @@ export const ProfilePage = () => {
         }
         setHasLoaded(false);
         fetchProfile();
-    },[id])
+    },[id, currentUser])
 
   return (
     <Col xs={10} md={6} className="mx-auto">
@@ -38,8 +42,8 @@ export const ProfilePage = () => {
                     <div className={styles.avatar}>
                         <Avatar src={image} height="100"></Avatar>
                     </div>
-                    {is_owner ? (<span className='d-flex ms-auto'><EllipsisButton className="mt-2"/></span>) : ("")}
-                    <Card.Title className={styles.underAvatar}>{owner}</Card.Title>
+                    {is_owner ? (<span className='d-flex ms-auto'><EllipsisProfileButton id={profile?.id} className="mt-2" setShowEditUserNameForm={setShowEditUserNameForm}/></span>) : ("")}
+                    {showEditUserNameForm ? (<UsernameForm setShowEditUserNameForm={setShowEditUserNameForm} profile={profile} setProfile={setProfile}/>) : (<Card.Title className={styles.underAvatar}>{owner}</Card.Title>)}
                     {name && <Card.Subtitle className="mb-2 text-muted">{name}</Card.Subtitle>}
                     <Card.Text className='mb-2 mt-3 '>About me:</Card.Text>
                     <Card.Text>
---
+++ src/pages/profiles/UsernameForm.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+
+import { useNavigate, useParams } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../contexts/CurrentUserContext";
+
+const UsernameForm = ({setShowEditUserNameForm}) => {
+  const [username, setUsername] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  useEffect(() => {
+    if (currentUser?.profile_id?.toString() === id) { 
+      setUsername(currentUser.username);
+    } else {
+        navigate("/");
+    }
+    console.log(currentUser)
+  }, [currentUser, navigate, id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put("/dj-rest-auth/user/", {
+        username,
+      });
+      setCurrentUser((prevUser) => ({
+        ...prevUser,
+        username : username,
+      }));
+      setShowEditUserNameForm(false);
+    } catch (err) {
+      console.log(err);
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Row>
+      <Col className="py-2 mx-auto text-center" md={6}>
+        <Container>
+          <Form onSubmit={handleSubmit} className="my-2">
+            <Form.Group>
+              <Form.Label>Change username</Form.Label>
+              <Form.Control
+                placeholder="username"
+                type="text"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </Form.Group>
+            {errors?.username?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+            <Button
+              className="me-2 mt-2"
+              onClick={() => setShowEditUserNameForm(false)}
+            >
+              cancel
+            </Button>
+            <Button
+                className="ms-2 mt-2"
+              type="submit"
+            >
+              save
+            </Button>
+          </Form>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default UsernameForm;
