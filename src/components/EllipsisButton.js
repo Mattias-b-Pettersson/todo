10915b1bd98b2e47c52b0c9c0148b88a138a095f
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-19 20:48:17 +0000

    Add profile button

--- src/components/EllipsisButton.js
+++ src/components/EllipsisButton.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { faEllipsisV, faFilePen, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisV, faFilePen, faTrashCan, faKey, faEdit, faIdCard } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, Dropdown } from 'react-bootstrap';
 import styles from "../styles/EllipsisButton.module.css"
+import { useNavigate } from 'react-router-dom';
+
 
 export const EllipsisButton = ({ handleDelete, handleEdit }) => {
             
@@ -36,5 +38,37 @@ export const EllipsisButton = ({ handleDelete, handleEdit }) => {
   )
 }
 
+export function EllipsisProfileButton({ id, setShowEditUserNameForm}) {
+  const navigate = useNavigate();
 
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <Button
+      variant='link'
+      className=""
+      href=""
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      {children}
+      <FontAwesomeIcon icon={faEllipsisV} className="color-black" />
+    </Button>
+    ));
 
+  return (
+    <Dropdown drop="left">
+      <Dropdown.Toggle as={CustomToggle} id="dropdown-basic" >
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu className={styles.menuOuter}>
+        <Dropdown.Item className={styles.menuItem} onClick={() => navigate(`/profiles/${id}/edit`)} ><span className='w-100 mx-2'><FontAwesomeIcon icon={faEdit} alt="edit" className='me-2' />Edit profile</span></Dropdown.Item>
+        <Dropdown.Divider className='p-0 m-0'/>
+        <Dropdown.Item className={styles.menuItem} onClick={() => setShowEditUserNameForm(true)}><span className='w-100 mx-2'><FontAwesomeIcon icon={faIdCard} alt="edit" className='me-2' />Edit username</span></Dropdown.Item>
+        <Dropdown.Divider className='p-0 m-0'/>
+        <Dropdown.Item className={styles.menuItem} onClick={() => navigate(`/profiles/${id}/edit/password`)}><span className='w-100 mx-2'><FontAwesomeIcon icon={faKey} alt="Change password" className='me-2' />Change password</span></Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  )
+}
