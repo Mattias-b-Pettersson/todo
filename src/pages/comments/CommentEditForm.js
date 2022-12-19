5a9f6cabcb0d74614d22ad6a9aedee59d4d5cce0
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-19 13:08:48 +0000

    Remove imported styles from commentedit form

--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import { axiosRes } from "../../api/axiosDefaults";
-import styles from "../../styles/CommentCreateEditForm.module.css";
 
 function CommentEditForm({ id, content, setShowEditForm, setComments }) {
 
@@ -40,7 +39,6 @@ function CommentEditForm({ id, content, setShowEditForm, setComments }) {
     <Form onSubmit={handleSubmit}>
       <Form.Group className="pr-1">
         <Form.Control
-          className={styles.Form}
           as="textarea"
           value={formContent}
           onChange={handleChange}
