acaf69ff1be4b95657707780aa41fea1ad573d88
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-20 20:23:07 +0000

    Fix bug where created by you view displays all todo

--- src/pages/todo/TodosPage.js
+++ src/pages/todo/TodosPage.js
@@ -122,7 +122,7 @@ export default function TodosPage () {
                                     <Col>
                                     <Form.Control name="assignedOrCreated" as="select" onChange={handleChange} value={assignedOrCreated} className='mt-2'>
                                             <option value={`&assigned=${currentUser?.profile_id}`}>Assigned to you</option>
-                                            <option value={`&created=${currentUser?.profile_id}`}>Created by you</option>
+                                            <option value={`&owner=${currentUser?.profile_id}`}>Created by you</option>
 
                                         </Form.Control>
                                     </Col>
